c4896f3cb5aed20b53a518411bf1e0a24698f58d
Oli Gibson <oligibson1@gmail.com>
2016-12-21 10:34:11 +0000

    Serve .tmp by default

--- generators/app/templates/server/routes.js
+++ generators/app/templates/server/routes.js
@@ -10,7 +10,8 @@ const path = require('path');
 module.exports = function(app) {
 
 <% if (client !== 'none') { -%>
-    app.use(express.static(path.resolve('../client')));
+    app.use(express.static(path.resolve('.tmp')));
+    app.use(express.static(path.resolve('client')));
 
     // All other routes should redirect to the index.html
     app.route('/*')
